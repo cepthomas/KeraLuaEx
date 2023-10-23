e2cefc8873c04c3ba4e4029f1a09d661d0c86a75
cepthomas <cepthomas@gmail.com>
2023-10-23 11:06:33 -0500

    WIP.

--- Test/scripts/foo.lua
+++ Test/scripts/foo.lua
@@ -3,13 +3,13 @@ local m = require("module1")
 
 function method1()
     local x = 10;
-    print("method 1")
+    print("method1()")
     m.method3(x)
 end
 
 function method2()
     local x = 10;
-    print("method 1")
+    print("method2()")
     method1()
 end
 
--- Test/scripts/module1.lua
+++ Test/scripts/module1.lua
@@ -3,7 +3,7 @@
 local module1 = { }
 
 function module1.method3(param)
-    print("method 3")
+    print("method3()")
     local y = 10 + param;
     return y * 2
 end
